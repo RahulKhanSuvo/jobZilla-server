6a0cda7aa79437a7392d3606bb7f19a998adaf75
rahulkhansuvo <rahul.khan.suvo@gmail.com>
2026-02-21 23:35:38 +0600

    feat:add

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,19 @@
-import express from "express"
-import cors from "cors"
-import routes from "./routes"
-import { notFound } from "./middleware/notFoundLogger"
-import { errorHandler } from "./middleware/globalErrorHandler"
-const app = express()
-app.use(express.json())
-app.use(cors())
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+import { notFound } from "./middleware/notFoundLogger";
+import { errorHandler } from "./middleware/globalErrorHandler";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
 app.get("/", (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "server is running"
-    })
-})
-app.use("/api/v1", routes)
+  res.status(200).json({
+    success: true,
+    message: "server is running",
+  });
+});
+app.use("/api/v1", routes);
 app.use(notFound);
-app.use(errorHandler)
-export default app
+app.use(errorHandler);
+export default app;
--- src/middleware/notFoundLogger.ts
+++ src/middleware/notFoundLogger.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Request, Response, NextFunction } from "express";
 
 export function notFound(req: Request, res: Response, next: NextFunction) {
-    res.status(404).json({
-        success: false,
-        message: `Not Found - ${req.originalUrl}`,
-    });
+  res.status(404).json({
+    success: false,
+    message: `Not Found - ${req.originalUrl}`,
+  });
 }
