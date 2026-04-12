56a99851d031d03a5b8f36813ac824db7ae1afd8
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-04-12 15:41:56 +0600

    feat: include user name and candidate location in application service query results

--- src/modules/application/application.service.ts
+++ src/modules/application/application.service.ts
@@ -95,6 +95,16 @@ const getAllApplications = async (userId: string) => {
           },
         },
       },
+      user: {
+        select: {
+          name: true,
+          candidate: {
+            select: {
+              location: true,
+            },
+          },
+        },
+      },
       resume: {
         select: {
           title: true,
